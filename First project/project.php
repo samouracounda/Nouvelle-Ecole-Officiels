abba639bb6ebc7e000eb51755f6e5bd5c36815b6
samouracounda <sadiobambosamoura037@gmail.com>
2024-07-20 17:11:57 +0000

    Add files via upload

---
+++ First project/project.php
@@ -0,0 +1,15 @@
+Salut ptt
+<?php
+$nom ='Pablo';
+echo $nom;
+$note = 10;
+$note = 15;
+echo ($note + $note2) / 2;
+?>
+
+C comment ?
+<?php
+$prenom ='Juan';
+$nom ='Pablo';
+echo $prenom . $nom;
+?>
